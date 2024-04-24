814615c19cc75e34844deb6ffd67ec4a0ad0f99d
samuel e. <49312226+dejazzhands@users.noreply.github.com>
2024-04-24 15:14:11 +0000

    added an array

--- refinedc-project/char_comparison.c
+++ refinedc-project/char_comparison.c
@@ -1,5 +1,15 @@
 #include <stdbool.h>
 
+
+int arr[26];
+// populated with ascii number representations
+for(int i = 65; i <= 90; i++){
+    arr[i - 65] = i;
+}
+
+
+
+
 [[rc::parameters("c : nat")]]
 [[rc::args("c @ int<u8>")]]
 [[rc::returns("{bool_decide (c = 97%nat)} @ builtin_boolean")]]
